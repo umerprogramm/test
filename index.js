3e529579d1ab8354b98f3296e77f34f6c2a21267
Your Name <you@example.com>
2023-04-05 20:48:35 +0500

    some changes

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var app = express();
+const nodemailer = require('nodemailer');
 
 app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
+
+
+app.post('/mail', async (req , res)=>{
+   console.log(req.body)
+  var transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+          user: 'umerprogrammer@gmail.com',
+          pass: 'kfbwoukjnvdulnek'
+      }
+  });
+  
+  console.log('created');
+  transporter.sendMail({
+  from: 'umerprogrammer@gmail.com',
+    to: 'umerprogrammer@gmail.com',
+    subject: 'hello world!',
+    text: 'hello world!'
+  });
+
+})
 
 
 var server = app.listen(3000, function () {
